657be96af6c506175e747982cae309caff5dac74
lroyce42 <lroyce42@aol.com>
2018-01-21 23:28:09 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped "+ cardOne);
+
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === 2)
+{
+  if(cardsInPlay[0] === cardsInPlay[1])
+  {
+    alert("You found a match!");
+  }
+  else {
+    {
+      alert("Sorry, try again!");
+    }
+  }
+}
